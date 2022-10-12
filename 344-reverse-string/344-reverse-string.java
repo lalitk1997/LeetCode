76f92f33731cdd0e73e3641abb768518a946fe9b
Lalit Kumar <lalit.k1997@gmail.com>
2022-10-13 00:00:17 +0530

    Time: 8 ms (5.26%), Space: 57.3 MB (34.51%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int ptrf = 0;
+        int ptre = s.length-1;
+        while(ptrf < ptre){
+            char temp = s[ptrf];
+            s[ptrf] = s[ptre];
+            s[ptre] = temp;
+            ptrf += 1;
+            ptre -= 1;
+        }
+        System.out.println(s.toString());
+    }
+}
