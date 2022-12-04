9f982e52822b82ad6029ed44ec991976266651e3
Lalit Kumar <lalit.k1997@gmail.com>
2022-12-04 12:06:51 +0530

    Time: 1618 ms (7.03%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
 
-SELECT T.name AS NAME, T.balance AS BALANCE FROM 
-(
-    SELECT Users.name, SUM(Transactions.amount) AS Balance 
-    FROM Users JOIN Transactions 
-    ON Users.account = Transactions.account
-    GROUP BY Users.account
-) AS T
-WHERE T.Balance > 10000;
+# SELECT T.name AS NAME, T.balance AS BALANCE FROM 
+# (
+#     SELECT Users.name, SUM(Transactions.amount) AS Balance 
+#     FROM Users JOIN Transactions 
+#     ON Users.account = Transactions.account
+#     GROUP BY Users.account
+# ) 
+# AS T
+# WHERE T.Balance > 10000;
+
+SELECT 
+u.name AS NAME,
+SUM(amount) as BALANCE 
+FROM users u 
+JOIN Transactions t 
+ON u.account = t.account  
+GROUP BY t.account  
+HAVING SUM(amount)>10000;
