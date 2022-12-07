94b86203631cfcf5e78520eb3265513c236f0768
Lalit Kumar <lalit.k1997@gmail.com>
2022-12-07 07:53:27 +0530

    Time: 3 ms (66.29%), Space: 42.6 MB (46.93%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,7 +1,7 @@
 class Solution {
     public String truncateSentence(String s, int k) {
         String[] strArray = s.split(" ");
-        System.out.println(Arrays.toString(strArray));
+        // System.out.println(Arrays.toString(strArray));
         StringBuilder strb = new StringBuilder();
         for(int i=0; i<k; i++){
             if(i == k-1){
@@ -10,7 +10,7 @@ class Solution {
                 strb.append(strArray[i]+" ");
             }
         }
-        System.out.println(strb);
+        // System.out.println(strb);
         return (strb.toString());
     }
     
