4c5048ad3d92dc5f4f9a80763c5cff4337030815
Lalit Kumar <lalit.k1997@gmail.com>
2024-02-23 18:31:23 +0530

    Time: 7 ms (18.04%) | Memory: 50.6 MB (59.03%) - LeetSync

--- 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
@@ -3,19 +3,12 @@ class Solution {
         int ans = 0;
         
         for(int i=0; i<items.size(); i++){
-            if( ( ruleKey.equals("type") ) && ( ruleValue.equals((items.get(i).get(0))) ) ){
-                //System.out.println(((items.get(0)).get(0)));
+            if( ( ruleKey.equals("type") ) && ( ruleValue.equals((items.get(i).get(0))) ) )
                 ans += 1;
-                continue;
-            }
-            if( ( ruleKey.equals("color") ) && ( ruleValue.equals((items.get(i).get(1))) ) ){
+            if( ( ruleKey.equals("color") ) && ( ruleValue.equals((items.get(i).get(1))) ) )
                 ans += 1;
-                continue;
-            }
-            if( ( ruleKey.equals("name") ) && ( ruleValue.equals((items.get(i).get(2))) ) ){
+            if( ( ruleKey.equals("name") ) && ( ruleValue.equals((items.get(i).get(2))) ) )
                 ans += 1;
-                continue;
-            }
         }
         return ans;
     }
