b2591245a58cb905dfaf56420b442f888764cd75
Lalit Kumar <lalit.k1997@gmail.com>
2024-02-25 01:48:57 +0530

    Time: 23 ms (17.32%) | Memory: 41.4 MB (65.41%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
@@ -1,10 +1,12 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
+
+        
         //Brute-Force
         int count = 0;
         for(int i=0; i<nums.length; i++){
-            for(int j=0; j<nums.length; j++){
-                for(int k=0; k<nums.length; k++){
+            for(int j=i; j<nums.length; j++){
+                for(int k=j; k<nums.length; k++){
                     if(i < j && j < k){
                         if((nums[j] - nums[i] == diff) && (nums[k] - nums[j] == diff))
                             count++;
@@ -13,5 +15,6 @@ class Solution {
             }
         }
         return count;
+        
     }
 }
