83868481a51024c6fe566ec503e0d4bf6239d65a
Lalit Kumar <lalit.k1997@gmail.com>
2024-02-24 22:54:00 +0530

    Time: 3 ms (87.44%) | Memory: 44.3 MB (26.92%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -3,6 +3,20 @@ class Solution {
         int maxCount = 0;
         for(int i=0; i<sentences.length; i++){
             int count = 0;
+            String[] tempStr = sentences[i].split(" ");
+            count = tempStr.length;
+            if(count > maxCount){
+                maxCount = count;
+            }
+        }
+        return (maxCount);
+        
+
+        /*
+        //Unoptimized
+        int maxCount = 0;
+        for(int i=0; i<sentences.length; i++){
+            int count = 0;
             for(int j=0; j<sentences[i].length(); j++){
                 if(sentences[i].charAt(j) == ' '){
                     count++;
@@ -13,5 +27,6 @@ class Solution {
             }
         }
         return (maxCount+1);
+        */
     }
 }
