fe65033eaf86f2c0b6343cae96082bf86003e190
Lalit Kumar <lalit.k1997@gmail.com>
2024-03-09 17:29:04 +0530

    Time: 4 ms (99.58%) | Memory: 44.8 MB (62.43%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -1,10 +1,38 @@
 class Solution {
     public int findNonMinOrMax(int[] nums) {
+        int max = -1;
+        int min = 999;
+        for(int i=0; i<nums.length; i++){
+            if(max < nums[i]){
+                max = nums[i];
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(min > nums[i]){
+                min = nums[i];
+            }
+        }
+        //System.out.println("Max : "+max);
+        //System.out.println("Min : "+min);
+        if(nums.length > 2){
+            for(int i=0; i<nums.length; i++){
+                if(nums[i] < max && nums[i] > min){
+                    return nums[i];
+                }
+            }
+            return -1;
+        }else{
+            return -1;
+        }
+
+        /*
+        // Time complexity O(nlogn) Space Complexity Logn
         if(nums.length > 2){
             Arrays.sort(nums);
             return nums[1];
         }else{
             return -1;
         }
+        */
     }
 }
